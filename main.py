66da12ffcde4ebbdf8c8498827d6d96b7c2add1a
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-10-02 23:05:55 +0300

    First task

---
+++ main.py
@@ -0,0 +1,13 @@
+# task 1
+string = input("Enter a string of characters: ")
+
+letter_count = 0
+digit_count = 0
+
+for chr in string:
+    if chr.isalpha():
+        letter_count += 1
+    elif chr.isdigit():
+        digit_count += 1
+print("Number of letters in the string:", letter_count)
+print("Number of digits in the string:", digit_count)
