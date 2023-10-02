151c4dec25e59cf738548aaf0a06738f19eff73f
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-10-02 23:25:22 +0300

    Second task

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 # task 1
-string = input("Enter a string of characters: ")
+# string = input("Enter a string of characters: ")
+#
+# letter_count = 0
+# digit_count = 0
+#
+# for chr in string:
+#     if chr.isalpha():
+#         letter_count += 1
+#     elif chr.isdigit():
+#         digit_count += 1
+# print("Number of letters in the string:", letter_count)
+# print("Number of digits in the string:", digit_count)
 
-letter_count = 0
-digit_count = 0
-
-for chr in string:
-    if chr.isalpha():
-        letter_count += 1
-    elif chr.isdigit():
-        digit_count += 1
-print("Number of letters in the string:", letter_count)
-print("Number of digits in the string:", digit_count)
+# task 2
+# string = input("Enter a string of characters: ")
+# search_chr = input("Enter the character you want to find in the string: ")
+#
+# count = 0
+#
+# for chr in string:
+#     if chr == search_chr:
+#         count += 1
+#         print(f"The character has been located {count} time(s)")
+#     else:
+#         print("The character has not been found")
+#         break
